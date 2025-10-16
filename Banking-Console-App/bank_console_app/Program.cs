b96363170786843a12880518d94c27847570cb2f
Garrett <wicketspaw@gmail.com>
2025-10-15 20:22:29 -0700

    Started work on bank console app in C#
    Made menu of selections that print to console. Started coding if statements for first two selections

---
+++ Banking-Console-App/bank_console_app/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Bank
+{
+    class Account
+    {
+        static void Main(string[] args)
+        {
+            int balance = 0;
+            int pin = 0488;
+
+            string Name = "Garrett's Checking";
+            //Print greeting and console options to choose from
+            Console.WriteLine("Welcome to C# Bank " + Name);
+            Console.WriteLine("1. Check Balance");
+            Console.WriteLine("2. Make A Deposit");
+            Console.WriteLine("3. Make A Withdrawl");
+
+            int Choice = Convert.ToInt32(Console.ReadLine());
+
+            if (Choice == 1)
+            {
+                Console.WriteLine("Enter pin to show balance: ");
+
+                int PinInput = Convert.ToInt32(Console.ReadLine());
+
+                if (PinInput == pin)
+                {
+                    Console.WriteLine("Login Success!");
+
+                    Console.WriteLine("Your Balance is: $" + balance);
+                }
+
+            }
+
+            else if (Choice == 2)
+            {
+                Console.WriteLine("Enter pin to make a deposit: ");
+
+                int PinInput = Convert.ToInt32(Console.ReadLine());
+
+                if (PinInput == pin)
+                {
+                    Console.WriteLine("Login Success!");
+
+                    Console.WriteLine("Please enter an amount you wish to deposit: $");
+
+                    int AnyAmount = Convert.ToInt32(Console.ReadLine());
+
+                    if (AnyAmount > balance)
+                    {
+                        Console.WriteLine("You have deposited: $" + AnyAmount);
+                        balance += AnyAmount;
+
+                        Console.WriteLine("Your new balance is: $" + balance + "!");
+                    }
+                }
+
+            }
+            
+            else if (Choice == 3)
+            {
+                
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Personal-Projects-Repo
-This repository is for my personal projects overseen by mentor
+This repository is for my personal projects overseen by mentor and projects authored by myself
+
+### Banking Console App
+
+- Barebones console app:
+    - Coded w/ C#
+    - Menu to deposit and withdraw balances
+    - User be able to specify which opptions
+    - pronts current balance
