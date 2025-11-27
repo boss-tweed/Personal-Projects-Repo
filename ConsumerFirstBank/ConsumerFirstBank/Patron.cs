1b60a7db717a8fd498afa0b32ee42379f94702b2
Garrett <wicketspaw@gmail.com>
2025-11-26 17:49:07 -0800

    Sent to review branch for merging to main (#9)

--- ConsumerFirstBank/ConsumerFirstBank/Patron.cs
+++ ConsumerFirstBank/ConsumerFirstBank/Patron.cs
@@ -12,23 +12,15 @@ namespace ConsumerFirstBank
         public string Name { get; set; }
         public int Pin { get; set; }
         public int AccountNumber { get; set; }
+        public string PatronId { get; set; }
+        public List<Account> Accounts { get; set; } = new List<Account>();
 
         public Patron(string name, int validPin)
         {
             Name = name;
             Pin = validPin;
-            AccountNumber = RandomAcctNum();
-        }
-
-        //Generates a random 6-digit account number
-        public static int RandomAcctNum()
-        {
-            return new Random().Next(100000, 999999);
-        }
-
-        public void AssignAcctNum()
-        {
-            AccountNumber = RandomAcctNum();
+            // Assign unique patron id
+            PatronId = UniqueIdGenerator.Instance.NextPatronId();
         }
 
     }
